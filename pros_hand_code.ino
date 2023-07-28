52ed3d6ea17e31fea61cfc6fa1d4945dacd263d0
VanessaNoel <55930144+VanessaNoel@users.noreply.github.com>
2023-07-28 10:52:54 -0400

    Update pros_hand_code.ino
    Went from 5 motors to two

--- pros_hand_code.ino
+++ pros_hand_code.ino
@@ -2,56 +2,34 @@
 
 //setting up button pins:
 const int buttonThumb = 2;
-const int buttonIndex = 4;
-const int buttonMiddle = 7; 
-const int buttonRing = 8;
-const int buttonPinky = 12;
+const int buttonFingers = 4;
 
 //setting up servo pins:
 const int servoThumb = 3;
-const int servoIndex = 5;
-const int servoMiddle = 6;
-const int servoRing = 9;
-const int servoPinky = 10;
+const int servoFingers = 5;
 
 //init servos:
 Servo thumbServo;
-Servo indexServo;
-Servo middleServo;
-Servo ringServo;
-Servo pinkyServo;
+Servo fingersServo;
 
 void setup() {
   //setting the buttons to input
   pinMode(buttonThumb, INPUT_PULLUP);
-  pinMode(buttonRing, INPUT_PULLUP);
-  pinMode(buttonMiddle, INPUT_PULLUP);
-  pinMode(buttonIndex, INPUT_PULLUP);
-  pinMode(buttonPinky, INPUT_PULLUP);
+  pinMode(buttonFingers, INPUT_PULLUP);
 
   //attaching the servos to the pins
   thumbServo.attach(servoThumb);
-  ringServo.attach(servoRing);
-  middleServo.attach(servoMiddle);
-  indexServo.attach(servoIndex);
-  pinkyServo.attach(servoPinky);
+  fingersServo.attach(servoFingers);
 }
 
 void loop() {
   //init all the button state variables:
   int buttonStateThumb;
-  int buttonStateRing;
-  int buttonStateMiddle;
-  int buttonStateIndex;
-  int buttonStatePinky;
+  int buttonStateFingers;
 
   //reading the buttons' state at each pin:
   buttonStateThumb = digitalRead(buttonThumb);
-  buttonStateRing = digitalRead(buttonRing);
-  buttonStateMiddle = digitalRead(buttonMiddle);
-  buttonStateIndex = digitalRead(buttonIndex);
-  buttonStatePinky = digitalRead(buttonPinky);
-
+  buttonStateFingers = digitalRead(buttonFingers);
 
   //YOU WILL NEED TO MODIFY THE CODE BASED ON YOUR SERVO ORIENTATION!!!
   
@@ -64,41 +42,13 @@ void loop() {
     thumbServo.write(180);
   }
   
-  //index finger pressed:
-  if (buttonStateIndex == LOW){
-    indexServo.write(0);
+  //fingers button pressed:
+  if (buttonStateFingers == LOW){
+    fingersServo.write(0);
   }
 
-  if (buttonStateIndex == HIGH) {
-    indexServo.write(180);
-  }
-  
-  
-  //middle finger pressed:
-  if (buttonStateMiddle == LOW){
-    middleServo.write(180);
-  }
-
-  if (buttonStateMiddle == HIGH) {
-    middleServo.write(0);
-  }
-  
-  //ring finger pressed:
-  if (buttonStateRing == LOW){
-    ringServo.write(180);
-  }
-
-  if (buttonStateRing == HIGH) {
-    ringServo.write(0);
-  }
-
-  //pinky finger pressed:
-  if (buttonStatePinky == LOW){
-    pinkyServo.write(180);
-  }
-
-  if (buttonStatePinky == HIGH) {
-    pinkyServo.write(0);
+  if (buttonStateFingers == HIGH) {
+    fingersServo.write(180);
   }
   
 }
